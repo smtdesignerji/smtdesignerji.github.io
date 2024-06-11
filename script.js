ac9e88947cb89f19022c539765e735a3afabf325
smtdesignerji <155347443+smtdesignerji@users.noreply.github.com>
2024-06-12 00:17:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+        // $('#menu-btn').click(function(){
+        //     $('nav .navication ul').addClass('active')
+
+        // })
+        // $('#menu-close').click(function(){
+        //     $('nav .navication ul').removeClass('active')
+
+        // })
+   
